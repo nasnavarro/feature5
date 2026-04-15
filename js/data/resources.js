61f831b6e3336b0f613576b461450eaf46358589
Ignacio <pub@valdecantos.com>
2026-04-15 17:33:38 +0200

    Array de estructura de datos de ejemplo de recursos

---
+++ js/data/resources.js
@@ -0,0 +1,44 @@
+export const resources = [
+    {
+        category: "Documentación Oficial",
+        items: [
+            { title: "Titulo ejemplo Documentación Oficial", url: "http://url-ejemplo-documentacion-oficial.com", note: "Descripción de la documentación oficial" },
+            { title: "Titulo 2 ejemplo Documentación Oficial", url: "http://url-ejemplo2-documentacion-oficial.com", note: "Descripción 2 de la documentación oficial" },
+        ]
+    },
+    {
+        category: "Aprendizaje",
+        items: [
+            { title: "Titulo ejemplo Aprendizaje", url: "http://url-ejemplo-aprendizaje.com", note: "Descripción de los recursos de aprendizaje" },
+            { title: "Titulo 2 ejemplo Aprendizaje", url: "http://url-ejemplo2-aprendizaje.com", note: "Descripción 2 de los recursos de aprendizaje" },
+        ]
+    },
+    {
+        category: "Herramientas",
+        items: [
+            { title: "Titulo ejemplo Herramientas", url: "http://url-ejemplo-herramientas.com", note: "Descripción de las herramientas disponibles" },
+            { title: "Titulo 2 ejemplo Herramientas", url: "http://url-ejemplo2-herramientas.com", note: "Descripción 2 de las herramientas disponibles" },
+        ]
+    },
+    {
+        category: "Accesibilidad",
+        items: [
+            { title: "Titulo ejemplo Accesibilidad", url: "http://url-ejemplo-accesibilidad.com", note: "Descripción de los recursos de accesibilidad" },
+            { title: "Titulo 2 ejemplo Accesibilidad", url: "http://url-ejemplo2-accesibilidad.com", note: "Descripción 2 de los recursos de accesibilidad" },
+        ]
+    },
+    {
+        category: "Blogs & Comunidades",
+        items: [
+            { title: "Titulo ejemplo Blogs & Comunidades", url: "http://url-ejemplo-blogs-comunidades.com", note: "Descripción de los blogs y comunidades" },
+            { title: "Titulo 2 ejemplo Blogs & Comunidades", url: "http://url-ejemplo2-blogs-comunidades.com", note: "Descripción 2 de los blogs y comunidades" },
+        ]
+    },
+    {
+        category: "Inspiración & Design",
+        items: [
+            { title: "Titulo ejemplo Inspiración & Design", url: "http://url-ejemplo-inspiracion-design.com", note: "Descripción de los recursos de inspiración y diseño" },
+            { title: "Titulo 2 ejemplo Inspiración & Design", url: "http://url-ejemplo2-inspiracion-design.com", note: "Descripción 2 de los recursos de inspiración y diseño" },
+        ]
+    }
+];
